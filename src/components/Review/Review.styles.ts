e2c0888abd6c8acac618d845482d1849e0fbc179
Jutrong <leek71358@gmail.com>
2024-03-24 15:39:47 +0900

    Style: Textarea 공통 컴포넌트 생성

--- src/components/Review/Review.styles.ts
+++ src/components/Review/Review.styles.ts
@@ -4,38 +4,54 @@ export const Wrap = styled.div`
   padding: 40px 100px;
   padding-top: 20px;
 `;
+export const ContentWrap = styled.div`
+  border: 1px solid blue;
+  padding: 0 50px;
+`;
 export const HeaderWrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 40px;
-  padding: 0 10px;
+  height: 30px;
+  padding: 20px 30px;
+  padding-right: 10px;
+  background-color: ${({ theme }) => theme.colors.mainColor};
 `;
-export const Header = styled.div``;
+export const Header = styled.div`
+  font-size: 14px;
+`;
 export const CloseBtn = styled.img`
-  width: 20px;
-  height: 20px;
-  margin: 10px 10px;
-  background-color: white;
+  width: 18px;
+  height: 18px;
+  margin: 10px;
   border-radius: 10%;
   cursor: pointer;
 `;
-export const InputWrap = styled.div``;
-export const ReviewInput = styled.textarea``;
+export const InputWrap = styled.div`
+  min-width: 320px;
+`;
+export const ReviewInput = styled.textarea`
+  height: 100px;
+`;
 export const StarRatingWrap = styled.div`
   display: flex;
   align-items: center;
-  gap: 10px;
+  gap: 50px;
+  margin-top: 20px;
+  font-size: 20px;
 `;
 export const ToiletPaperWrap = styled.div`
   display: flex;
   align-items: center;
-  gap: 10px;
+  gap: 20px;
+  margin-top: 10px;
+  font-size: 20px;
   div {
     display: flex;
   }
 `;
-export const SubmitBtn = styled.button`
-  border: 1px solid black;
+export const BtnWrap = styled.button`
+  width: 100%;
+  margin-top: 16px;
 `;
---
+++ src/components/shared/Textarea.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+
+type TextareaProps = {
+  label?: string;
+  placeholder: string;
+  value: string | number;
+  errorMessage?: string;
+  height?: string;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+const Textarea = ({ label, placeholder, value, errorMessage, height, onChange }: TextareaProps) => {
+  return (
+    <TextareaWrap>
+      <Label>{label}</Label>
+      <ElTextarea placeholder={placeholder} value={value} onChange={onChange} />
+      {errorMessage && <ErrorMessage>* {errorMessage}</ErrorMessage>}
+    </TextareaWrap>
+  );
+};
+
+const TextareaWrap = styled.div``;
+const ElTextarea = styled.textarea<TextareaProps>`
+   width: 100%;
+   height: ${({ height }) => height || '100px'};
+   border: 1px solid #d5d4dc;
+   border-radius: 5px;
+   padding: 20px 10px;
+   background: none;
+   outline: none;
+   resize: none;
+   &::placeholder {
+     top: 10%;
+     font-size: 12px;
+     opacity: 0.7;
+     font-family: 'Pretendard-ExtraLight';
+   }
+   &:focus {
+     border: 1px solid ${({ theme }) => theme.colors.mainColor};
+   }
+ `;
+const Label = styled.label`
+   display: block;
+   margin-bottom: 15px;
+   font-size: 16px;
+   font-family: 'Pretendard-SemiBold';
+ `;
+const ErrorMessage = styled.div`
+   font-size: 12px;
+   color: crimson;
+   margin-top: 8px;
+   margin-left: 4px;
+ `;
+export default Textarea;
