a03bd406b52a0dc950791d167dea839d81b34444
Jutrong <leek71358@gmail.com>
2024-03-25 20:51:55 +0900

    Style: swiper slide 간격 조절

--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -1,9 +1,9 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, FreeMode } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-// import { Pagination, Navigation } from 'swiper';
+import 'swiper/css/free-mode';
 import { Toilet } from "@_types/toilet";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
@@ -80,15 +80,18 @@ const PostDetail = () => {
         <KakaoStaticMap lat={post?.WGS84위도 || 0} lng={post?.WGS84경도 || 0} toiletName={post?.화장실명} />
         {reviews && reviews.length > 0 ? (
           <Swiper
-            modules={[Pagination, Navigation]}
-            spaceBetween={320} // 슬라이드 간의 간격
-            slidesPerView={3} // 한 번에 보여질 슬라이드 개수
-            pagination={{ clickable: true }} // 페이지네이션 활성화
-            navigation // 네비게이션 활성화
-            style={{ width: '100%', padding: '10px 0' }} // Swiper 컨테이너 스타일링
+            modules={[Pagination, Navigation, FreeMode]}
+            spaceBetween={40}
+            slidesPerView={'auto'}
+            centeredSlides={true}
+            pagination={{ clickable: true }}
+            rewind={true}
+            navigation
+            freeMode={true}
+            style={{ width: '100%', padding: '10px 0' }}
           >
             {reviews.map((review) => (
-              <SwiperSlide key={review.id}>
+              <SwiperSlide key={review.id} style={{ width: 'auto' }}>
                 <ReviewDisplay review={review} postId={id || ''} />
               </SwiperSlide>
             ))}
